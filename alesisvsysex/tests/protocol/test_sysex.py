133783dc248616f4e0345ed93c8e0c2c1b3d7d42
Alastair Bridgewater <alastair.bridgewater@gmail.com>
2018-06-02 12:03:56 -0400

    Duplicate out some V25-specific tests and adjust for VMini
      * If we're going to be supporting multiple device types at once,
    we should have tests for at least two of them.
    
      * We want to keep the existing tests, because they cover one of
    our target devices, but we also need versions that we an run now,
    so copy/paste/edit is in order.

--- alesisvsysex/tests/protocol/test_sysex.py
+++ alesisvsysex/tests/protocol/test_sysex.py
@@ -5,20 +5,36 @@ import unittest
 
 class TestSysex(unittest.TestCase):
     @unittest.skip("expects V25 message header")
-    def test_sysex_serialize_query(self):
+    def test_sysex_serialize_query_v25(self):
         q = SysexMessage('query')
         assert q.serialize() == bytes([0xf0, 0x00, 0x00, 0x0e, 0x00, 0x41, 0x62, 0x00, 0x5d, 0xf7])
 
+    def test_sysex_serialize_query_vmini(self):
+        q = SysexMessage('query')
+        assert q.serialize() == bytes([0xf0, 0x00, 0x00, 0x0e, 0x00, 0x49, 0x62, 0x00, 0x38, 0xf7])
+
     @unittest.skip("uses V25 message header")
-    def test_deserialize_query(self):
+    def test_deserialize_query_v25(self):
         b = bytes([0xf0, 0x00, 0x00, 0x0e, 0x00, 0x41, 0x62, 0x00, 0x5d, 0xf7])
         r = SysexMessage.deserialize(b)
         assert r.type == 'query'
 
+    def test_deserialize_query_vmini(self):
+        b = bytes([0xf0, 0x00, 0x00, 0x0e, 0x00, 0x49, 0x62, 0x00, 0x38, 0xf7])
+        r = SysexMessage.deserialize(b)
+        assert r.type == 'query'
+
     @unittest.skip("expects V25 message header")
-    def test_sysex_serialize_update(self):
+    def test_sysex_serialize_update_v25(self):
         q = SysexMessage('update', AlesisV())
         begin = bytes([0xf0, 0x00, 0x00, 0x0e, 0x00, 0x41, 0x61, 0x00, 0x5d])
+        end = bytes([0xf7])
+        res = q.serialize()
+        assert res[0:len(begin)] == begin and res[-len(end):] == end
+
+    def test_sysex_serialize_update_vmini(self):
+        q = SysexMessage('update', AlesisV())
+        begin = bytes([0xf0, 0x00, 0x00, 0x0e, 0x00, 0x49, 0x61, 0x00, 0x38])
         end = bytes([0xf7])
         res = q.serialize()
         assert res[0:len(begin)] == begin and res[-len(end):] == end
