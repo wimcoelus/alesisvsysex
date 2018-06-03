1e5b2144c28ee37ff78c2a6aaabbd98cc790a823
Alastair Bridgewater <alastair.bridgewater@gmail.com>
2018-06-02 21:38:32 -0400

    Auto-select "sane" initial model on startup
      * Rather than hardcoding the use of the VMini model, first check
    for connected devices and use a model compatible with the first
    such device found.  If there aren't any connected devices, use an
    arbitrary valid model (the "first" subclass of AlesisModel).
    
      * This is mostly trying for the "pleasant surprise" effect of
    managing to do something sane given the available hardware when
    the program starts, but may end up confusing or annoying some
    people.  If necessary, we should be able to revert this easily
    enough.

--- alesisvsysex/ui/main.py
+++ alesisvsysex/ui/main.py
@@ -1,9 +1,18 @@
 from PyQt5.QtWidgets import QApplication
-from alesisvsysex.protocol.model import AlesisVMini
+from alesisvsysex.protocol.model import AlesisModel
+from alesisvsysex.device.alesis import AlesisMIDIDevice
 from alesisvsysex.ui.window import AlesisVSysexApplication
 
 def main(argv):
     app = QApplication(argv)
-    ex = AlesisVSysexApplication(AlesisVMini())
+    # For the default model, pick the class of the first connected
+    # device that we find, or an arbitrary valid model class if there
+    # is no connected device.
+    ports = AlesisMIDIDevice.findAllPorts()
+    if len(ports) > 0:
+        model_class = ports[0][1]
+    else:
+        model_class = AlesisModel.__subclasses__()[0]
+    ex = AlesisVSysexApplication(model_class())
     return app.exec_()
 
