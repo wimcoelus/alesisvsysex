c32709a13f8eb82b80f30680aaf1b3a4fadc9f6f
Alastair Bridgewater <alastair.bridgewater@gmail.com>
2018-06-04 22:51:10 -0400

    Restructure sysex message deserialization
      * Mostly using struct.unpack_from() to simplify the parsing code.
    
      * But also restructuring the message type dispatch the same way
    that we did for serialization four commits ago.

--- alesisvsysex/protocol/sysex.py
+++ alesisvsysex/protocol/sysex.py
@@ -1,3 +1,4 @@
+from struct import unpack_from
 from .model import AlesisModel
 
 __all__ = ['SysexMessage']
@@ -58,42 +59,37 @@ class SysexMessage (object):
     @classmethod
     def deserialize(cls, b):
         i = 0
-        
-        start_byte = b[i : i+1]
-        if start_byte != bytes(cls._START_BYTE):
-            raise ValueError("Invalid start byte '0x%02x'" % start_byte[0])
-        i += 1
-        
-        manufacturer = b[i : i + len(cls._MANUFACTURER_ALESIS)]
-        if manufacturer != bytes(cls._MANUFACTURER_ALESIS):
-            raise ValueError("Invalid manufacturer id")
-        i += len(cls._MANUFACTURER_ALESIS)
 
-        device_id = list(b[i : i + 2])
-        model_class = AlesisModel.findModelByDeviceId(device_id)
+        (start_byte, manufacturer, device_id, t, message_length) = unpack_from('B3s2sB2s', b, i)
+        i += 9
+
+        if [start_byte] != cls._START_BYTE:
+            raise ValueError("Invalid start byte '0x%02x'" % start_byte[0])
+        
+        if list(manufacturer) != cls._MANUFACTURER_ALESIS:
+            raise ValueError("Invalid manufacturer id")
+
+        model_class = AlesisModel.findModelByDeviceId(list(device_id))
         if model_class is None:
             raise ValueError("Invalid device id")
-        i += len(device_id)
         
-        t = b[i : i + 1]
         for k, v in cls._TYPES.items():
-            if t == bytes(v):
+            if t == v[0]:
                 msg_type = k
                 break
         else:
             raise ValueError("Unknown message type '0x%02x'" % t[0])
-        i += 1
-        
-        message_length = b[i : i + 2]
+
         if message_length != bytes(cls.encodeWord(model_class.num_bytes())):
             raise ValueError("Invalid message length")
-        i += len(message_length)
         
         if msg_type == "query":
             model = None
-        else:
+        elif (msg_type == 'update') or (msg_type == 'reply'):
             model = model_class.deserialize(b[i : i + model_class.num_bytes()])
             i += model_class.num_bytes()
+        else:
+            raise RuntimeError('Don\'t know how to decode %s messages' % msg_type)
         
         end_byte = b[i : i+1]
         if end_byte != bytes(cls._END_BYTE):
